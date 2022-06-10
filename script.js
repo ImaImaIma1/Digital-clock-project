7f69e36ad99280fa93bf313ff21e28b5f53eb9ea
Imani Stevenson <i.stevenson980@gmail.com>
2022-06-09 23:45:09 -0400

    -add project to folder

---
+++ script.js
@@ -0,0 +1,27 @@
+setInterval(digitalClock, 1000);
+
+function digitalClock() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var session = "AM";
+
+  if (hours == 0) {
+    hours = 12;
+  }
+
+  if (hours > 12) {
+    hours = hours - 12;
+    session = "PM";
+  }
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  var time = hours + ":" + minutes + ":" + seconds + " " + session;
+
+  document.getElementById("MyClockDisplay").innerHTML = time;
+}
+digitalClock();
